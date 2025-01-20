ee6efea85f2df5ce1f80c2db3b2afb5d91979ffd
youminki <dbalsrl7648@gmail.com>
2025-01-21 07:26:25 +0900

    fix : 배포오류 #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", // 상대 경로를 명시
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: true, // 디버깅을 위해 sourcemap 활성화
-  },
   resolve: {
     alias: {
-      "@": "/src", // 경로 별칭 설정
+      "@": "/src",
     },
   },
 });
