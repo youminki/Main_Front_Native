c4dd49464a81a6200afeba09c0510d7d293cacec
youminki <dbalsrl7648@gmail.com>
2025-02-14 23:11:52 +0900

    Update SettingMelpik.tsx

--- src/pages/Melpik/Setting/SettingMelpik.tsx
+++ src/pages/Melpik/Setting/SettingMelpik.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import Theme from '../../../styles/Theme';
 import StatsSection from '../../../components/Melpik/StatsSection';
+import InputField from '../../../components/InputField';
+import Button02 from '../../../components/Button02';
 
 const SettingMelpik: React.FC = () => {
   const visits = '@styleweex';
@@ -30,6 +32,77 @@ const SettingMelpik: React.FC = () => {
           />
         </StatsRow>
         <Divider />
+
+        <Section>
+          <InputField
+            label='멜픽 주소 (변경불가)'
+            id='melpickAddress'
+            type='text'
+            placeholder='주소를 입력하세요'
+            buttonLabel='체크'
+            buttonColor='yellow'
+            required
+            maxLength={12}
+            prefix='melpick.com/'
+          />
+
+          <InputField
+            placeholder='등록하실 링크를 추가하세요'
+            buttonLabel='등록/변경'
+            label='멜픽 자동생성 설정'
+            id='personal-link'
+            type='text'
+          />
+          <InputField
+            placeholder='등록하실 링크를 추가하세요'
+            buttonLabel='등록/변경'
+            label='멜픽 자동생성 설정'
+            id='personal-link'
+            type='text'
+          />
+
+          <InputField
+            placeholder='등록하실 링크를 추가하세요'
+            buttonLabel='링크등록'
+            label='개인 링크 설정 (선택)'
+            id='personal-link'
+            type='text'
+          />
+        </Section>
+
+        <Section>
+          <Label>정산 계좌정보 (필수)</Label>
+          <AccountInfo>4532**-**-***544 (국민)</AccountInfo>
+          <Button02>등록/변경</Button02>
+        </Section>
+
+        <Section>
+          <InputField
+            placeholder='등록하실 링크를 추가하세요'
+            buttonLabel='링크등록'
+            label='개인 링크 설정 (선택)'
+            id='personal-link'
+            type='text'
+          />
+          <LinkList>
+            <LinkItem>
+              <LinkText>밍또의 세상 | youtu.be/Kw482drmWqw</LinkText>
+              <DeleteButton>✕</DeleteButton>
+            </LinkItem>
+            <LinkItem>
+              <LinkText>서비스 소개 링크 | youtu.be/UfLf_60xa2a</LinkText>
+              <DeleteButton>✕</DeleteButton>
+            </LinkItem>
+            <LinkItem>
+              <LinkText>2024 티타임지 인터뷰 | myteatime.kr/con...</LinkText>
+              <DeleteButton>✕</DeleteButton>
+            </LinkItem>
+            <LinkItem>
+              <LinkText>2024 네이버 인터뷰 | myteatime.kr/cont1...</LinkText>
+              <DeleteButton>✕</DeleteButton>
+            </LinkItem>
+          </LinkList>
+        </Section>
       </Container>
     </ThemeProvider>
   );
@@ -43,28 +116,93 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   background-color: #fff;
-  font-family: 'NanumSquare Neo OTF', sans-serif;
 `;
 
 const Header = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
   width: 100%;
-  margin-bottom: 6px;
+  margin-bottom: 10px;
 `;
 
 const Title = styled.h1`
   font-size: 24px;
-  font-weight: 800;
+  font-weight: bold;
   color: #000;
-  margin-bottom: 0px;
 `;
 
 const Subtitle = styled.p`
   font-size: 12px;
-  font-weight: 400;
-  color: #ccc;
+  color: #aaa;
+`;
+
+const StatsSectionRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background: #ddd;
+  margin: 20px 0;
+`;
+
+const Section = styled.div`
+  width: 100%;
+  padding: 10px 0;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+const FixedInput = styled.div`
+  background: #f6f6f6;
+  padding: 10px;
+  border-radius: 5px;
+  width: 100%;
+`;
+
+const ToggleButton = styled.button`
+  background: black;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  width: 100%;
+`;
+
+const AccountInfo = styled.div`
+  font-weight: bold;
+  color: #f6ae24;
+  margin-bottom: 5px;
+`;
+
+const LinkList = styled.ul`
+  margin-top: 10px;
+  list-style: none;
+  padding: 0;
+`;
+
+const LinkItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  background: #f6f6f6;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 5px;
+`;
+
+const LinkText = styled.span`
+  font-size: 14px;
+`;
+
+const DeleteButton = styled.button`
+  background: none;
+  border: none;
+  color: red;
+  font-size: 16px;
+  cursor: pointer;
 `;
 
 const StatsRow = styled.div`
@@ -73,11 +211,4 @@ const StatsRow = styled.div`
   justify-content: space-between;
   width: 100%;
   padding: 0 20px;
-`;
-
-const Divider = styled.div`
-  width: 100%;
-  height: 1px;
-  background: #dddddd;
-  margin: 30px 0;
 `;
