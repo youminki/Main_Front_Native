7050a2da7535207b3bbc231c7b5e955e295aee40
youminki <dbalsrl7648@gmail.com>
2025-02-16 05:21:27 +0900

    feat : settlement-request UI

--- src/components/ReusableModal.tsx
+++ src/components/ReusableModal.tsx
@@ -44,16 +44,18 @@ export default ReusableModal;
 
 const StyledModal = styled.div`
   position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 27px;
   z-index: 9999;
+  width: 100vw;
+  height: 100vh;
+  max-width: 800px;
 `;
 
 const ModalContent = styled.div<{ width: string; height: string }>`
--- src/pages/Melpik/Calculate/SettlementRequest.tsx
+++ src/pages/Melpik/Calculate/SettlementRequest.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Theme from '../../../styles/Theme';
-import AgreementSection from '../../../components/Signup/AgreementSection';
+import AgreementSection from '../../../components/Melpik/settlement/SettlementAgreementSection';
 import InputField from '../../../components/InputField';
 import FixedBottomBar from '../../../components/FixedBottomBar';
 
