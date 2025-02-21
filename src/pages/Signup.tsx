a3da40340fb2d4ac87033b95940b35f590b8c723
youminki <dbalsrl7648@gmail.com>
2025-02-22 04:24:10 +0900

    fix : Yup check 검증 구조 수정 - ture and false

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { schemaSignup } from '../hooks/ValidationYup';
 import InputField from '../components/InputField';
@@ -12,6 +12,7 @@ import { signupUser } from '../api/user/signupPost';
 import { useNavigate } from 'react-router-dom';
 import { CustomSelect } from '../components/CustomSelect';
 
+// SignupFormData에 agreeToTerms와 agreeToPrivacyPolicy를 추가합니다.
 type SignupFormData = {
   email: string;
   password: string;
@@ -23,21 +24,36 @@ type SignupFormData = {
   region: string;
   district: string;
   melpickAddress: string;
-  instar?: string; // 선택적 필드로 수정
   agreeToTerms: boolean;
   agreeToPrivacyPolicy: boolean;
 };
 
 const Signup: React.FC = () => {
   const navigate = useNavigate();
+  // defaultValues에 약관 동의값도 추가합니다.
+  const methods = useForm<SignupFormData>({
+    resolver: yupResolver(schemaSignup),
+    mode: 'all',
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      nickname: '',
+      name: '',
+      birthYear: '',
+      phoneNumber: '',
+      region: '',
+      district: '',
+      melpickAddress: '',
+      agreeToTerms: false, // 초기값 false (필요에 따라 true로 변경)
+      agreeToPrivacyPolicy: false, // 초기값 false
+    },
+  });
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<SignupFormData>({
-    resolver: yupResolver(schemaSignup),
-    mode: 'all',
-  });
+  } = methods;
 
   const [gender, setGender] = useState<string>('여성');
   const [selectedGenderButton, setSelectedGenderButton] =
@@ -89,6 +105,7 @@ const Signup: React.FC = () => {
 
     setErrorMessage(null);
 
+    // formattedData에 agreeToTerms와 agreeToPrivacyPolicy를 함께 전달합니다.
     const formattedData = {
       email: data.email,
       password: data.password,
@@ -98,7 +115,6 @@ const Signup: React.FC = () => {
       address: `${data.region} ${data.district}`,
       phoneNumber: data.phoneNumber,
       gender: gender === '여성' ? 'female' : 'male',
-      instagramId: data.instar || undefined,
       agreeToTerms: data.agreeToTerms,
       agreeToPrivacyPolicy: data.agreeToPrivacyPolicy,
     };
@@ -122,181 +138,184 @@ const Signup: React.FC = () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <Container>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <AgreementSection />
+      <FormProvider {...methods}>
+        <Container>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            {/* AgreementSection 내부에서 useFormContext를 사용하여 체크박스를 등록 */}
+            <AgreementSection />
 
-          {errorMessage && <ErrorText>{errorMessage}</ErrorText>}
+            {errorMessage && <ErrorText>{errorMessage}</ErrorText>}
 
-          <InputField
-            label='계정(이메일)'
-            id='email'
-            type='text'
-            error={errors.email}
-            placeholder='계정을 입력하세요'
-            isEmailField
-            {...register('email')}
-            required
-            maxLength={20}
-            onButtonClick={handleInstagramCheck}
-          />
-
-          <InputField
-            label='비밀번호(숫자, 문자를 조합하여 8자리 이상 입력하세요)'
-            id='password'
-            type='password'
-            placeholder='비밀번호를 입력하세요'
-            error={errors.password}
-            {...register('password')}
-            required
-            maxLength={20}
-            autoComplete='current-password'
-          />
-
-          <InputField
-            label='비밀번호 확인'
-            id='passwordConfirm'
-            type='password'
-            placeholder='비밀번호를 한번 더 입력하세요'
-            error={errors.passwordConfirm}
-            {...register('passwordConfirm')}
-            required
-            maxLength={20}
-          />
-
-          <InputField
-            label='닉네임(8글자 이내)'
-            id='nickname'
-            type='text'
-            placeholder='닉네임을 입력하세요'
-            error={errors.nickname}
-            {...register('nickname')}
-            required
-            maxLength={8}
-            buttonLabel='중복확인'
-            onButtonClick={handleNicknameCheck}
-          />
-
-          <RowLabel>
             <InputField
-              label='이름'
-              id='name'
+              label='계정(이메일)'
+              id='email'
               type='text'
-              placeholder='이름을 입력하세요'
-              error={errors.name}
-              {...register('name')}
+              error={errors.email}
+              placeholder='계정을 입력하세요'
+              isEmailField
+              {...register('email')}
               required
-              maxLength={5}
+              maxLength={20}
+              onButtonClick={handleInstagramCheck}
             />
 
-            {/** as={CustomSelect}로 커스텀 화살표 적용 */}
             <InputField
-              label='태어난 해'
-              id='birthYear'
-              as={CustomSelect}
-              error={errors.birthYear}
+              label='비밀번호(숫자, 문자를 조합하여 8자리 이상 입력하세요)'
+              id='password'
+              type='password'
+              placeholder='비밀번호를 입력하세요'
+              error={errors.password}
+              {...register('password')}
               required
-              {...register('birthYear')}
-            >
-              {Array.from({ length: 100 }, (_, i) => 2023 - i).map((year) => (
-                <option key={year} value={year}>
-                  {year}년
-                </option>
-              ))}
-            </InputField>
-          </RowLabel>
-
-          <GenderField>
-            <InputFieldLabel>성별</InputFieldLabel>
-            <GenderRow>
-              <GenderButton
-                type='button'
-                selected={gender === '여성'}
-                onClick={() => handleGenderChange('여성')}
-                isSelected={selectedGenderButton === '여성'}
-              >
-                여성
-              </GenderButton>
-              <GenderButton
-                type='button'
-                selected={gender === '남성'}
-                onClick={() => handleGenderChange('남성')}
-                isSelected={selectedGenderButton === '남성'}
-              >
-                남성
-              </GenderButton>
-            </GenderRow>
-          </GenderField>
-
-          <PhoneField>
-            <InputField
-              label='전화번호(11자를 입력하세요)'
-              id='phoneNumber'
-              type='text'
-              placeholder='전화번호를 입력하세요'
-              error={errors.phoneNumber}
-              {...register('phoneNumber')}
-              required
-              maxLength={11}
-              onInput={handlePhoneNumberChange}
-              buttonLabel='본인인증'
-              onButtonClick={handleVerification}
+              maxLength={20}
+              autoComplete='current-password'
             />
-          </PhoneField>
-
-          <RowLabel>
-            <InputField
-              label='지역'
-              id='region'
-              as={CustomSelect}
-              error={errors.region}
-              required
-              {...register('region')}
-            >
-              <option value='서울특별시'>서울특별시</option>
-              <option value='경기도'>경기도</option>
-            </InputField>
 
             <InputField
-              label='구'
-              id='district'
-              as={CustomSelect}
-              error={errors.district}
+              label='비밀번호 확인'
+              id='passwordConfirm'
+              type='password'
+              placeholder='비밀번호를 한번 더 입력하세요'
+              error={errors.passwordConfirm}
+              {...register('passwordConfirm')}
               required
-              {...register('district')}
-            >
-              <option value='강남구'>강남구</option>
-              <option value='서초구'>서초구</option>
-              <option value='금천구'>금천구</option>
-            </InputField>
-          </RowLabel>
+              maxLength={20}
+            />
 
-          <InputField
-            label='멜픽 주소설정(영문, 숫자 12글자 이내)'
-            id='melpickAddress'
-            type='text'
-            placeholder='주소를 입력하세요'
-            error={errors.melpickAddress}
-            {...register('melpickAddress')}
-            value={melpickAddress}
-            onChange={handleMelpickAddressChange}
-            buttonLabel='체크'
-            buttonColor='yellow'
-            required
-            maxLength={12}
-            onButtonClick={handleCheckClick}
-            prefix='melpick.com/'
-          />
+            <InputField
+              label='닉네임(8글자 이내)'
+              id='nickname'
+              type='text'
+              placeholder='닉네임을 입력하세요'
+              error={errors.nickname}
+              {...register('nickname')}
+              required
+              maxLength={8}
+              buttonLabel='중복확인'
+              onButtonClick={handleNicknameCheck}
+            />
 
-          <BlackContainer />
-          <BottomBar
-            imageSrc={ResetButtonIcon}
-            buttonText={isSubmitting ? '가입 중...' : '회원가입'}
-            type='submit'
-            disabled={isSubmitting}
-          />
-        </Form>
-      </Container>
+            <RowLabel>
+              <InputField
+                label='이름'
+                id='name'
+                type='text'
+                placeholder='이름을 입력하세요'
+                error={errors.name}
+                {...register('name')}
+                required
+                maxLength={5}
+              />
+
+              {/** as={CustomSelect}로 커스텀 화살표 적용 */}
+              <InputField
+                label='태어난 해'
+                id='birthYear'
+                as={CustomSelect}
+                error={errors.birthYear}
+                required
+                {...register('birthYear')}
+              >
+                {Array.from({ length: 100 }, (_, i) => 2023 - i).map((year) => (
+                  <option key={year} value={year}>
+                    {year}년
+                  </option>
+                ))}
+              </InputField>
+            </RowLabel>
+
+            <GenderField>
+              <InputFieldLabel>성별</InputFieldLabel>
+              <GenderRow>
+                <GenderButton
+                  type='button'
+                  selected={gender === '여성'}
+                  onClick={() => handleGenderChange('여성')}
+                  isSelected={selectedGenderButton === '여성'}
+                >
+                  여성
+                </GenderButton>
+                <GenderButton
+                  type='button'
+                  selected={gender === '남성'}
+                  onClick={() => handleGenderChange('남성')}
+                  isSelected={selectedGenderButton === '남성'}
+                >
+                  남성
+                </GenderButton>
+              </GenderRow>
+            </GenderField>
+
+            <PhoneField>
+              <InputField
+                label='전화번호(11자를 입력하세요)'
+                id='phoneNumber'
+                type='text'
+                placeholder='전화번호를 입력하세요'
+                error={errors.phoneNumber}
+                {...register('phoneNumber')}
+                required
+                maxLength={11}
+                onInput={handlePhoneNumberChange}
+                buttonLabel='본인인증'
+                onButtonClick={handleVerification}
+              />
+            </PhoneField>
+
+            <RowLabel>
+              <InputField
+                label='지역'
+                id='region'
+                as={CustomSelect}
+                error={errors.region}
+                required
+                {...register('region')}
+              >
+                <option value='서울특별시'>서울특별시</option>
+                <option value='경기도'>경기도</option>
+              </InputField>
+
+              <InputField
+                label='구'
+                id='district'
+                as={CustomSelect}
+                error={errors.district}
+                required
+                {...register('district')}
+              >
+                <option value='강남구'>강남구</option>
+                <option value='서초구'>서초구</option>
+                <option value='금천구'>금천구</option>
+              </InputField>
+            </RowLabel>
+
+            <InputField
+              label='멜픽 주소설정(영문, 숫자 12글자 이내)'
+              id='melpickAddress'
+              type='text'
+              placeholder='주소를 입력하세요'
+              error={errors.melpickAddress}
+              {...register('melpickAddress')}
+              value={melpickAddress}
+              onChange={handleMelpickAddressChange}
+              buttonLabel='체크'
+              buttonColor='yellow'
+              required
+              maxLength={12}
+              onButtonClick={handleCheckClick}
+              prefix='melpick.com/'
+            />
+
+            <BlackContainer />
+            <BottomBar
+              imageSrc={ResetButtonIcon}
+              buttonText={isSubmitting ? '가입 중...' : '회원가입'}
+              type='submit'
+              disabled={isSubmitting}
+            />
+          </Form>
+        </Container>
+      </FormProvider>
     </ThemeProvider>
   );
 };
