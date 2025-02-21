bcbccc94ac9df6fa335b99308571bdd0e5a6ab6a
youminki <dbalsrl7648@gmail.com>
2025-02-22 06:57:36 +0900

    feat : LockerRoom UI

---
+++ src/pages/LockerRoom/LockerRoom.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import Theme from '../../styles/Theme';
+import StatsSection from '../../components/LockerRoom/StatsSection';
+import CardGrid from '../../components/LockerRoom/CardGrid';
+import LockerRoomIcons from '../../assets/LockerRoom/LockerRoomIcons.svg';
+
+const LockerRoom: React.FC = () => {
+  // 동적 데이터 (프롭스로 전달할 값)
+  const visits = '구독자';
+  const sales = '26,000';
+  const dateRange = '요약정보';
+
+  const visitLabel = '그룹';
+  const salesLabel = '보유 포인트';
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <Container>
+        <Header>
+          <Title>락커룸</Title>
+          <Subtitle>나에게 맞는 스타일을 찾을 때는 멜픽!</Subtitle>
+        </Header>
+
+        <StatsRow>
+          <StatsSection
+            visits={visits}
+            sales={sales}
+            dateRange={dateRange}
+            visitLabel={visitLabel}
+            salesLabel={salesLabel}
+          />
+          <ImageWrapper>
+            <MenuImage src={LockerRoomIcons} alt='메뉴 이미지' />
+          </ImageWrapper>
+        </StatsRow>
+        <Divider />
+        <CardGrid />
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default LockerRoom;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background-color: #fff;
+  font-family: 'NanumSquare Neo OTF', sans-serif;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  margin-bottom: 6px;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 800;
+  color: #000;
+  margin-bottom: 0px;
+`;
+
+const Subtitle = styled.p`
+  font-size: 12px;
+  font-weight: 400;
+  color: #ccc;
+`;
+
+const StatsRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 20px;
+`;
+
+const ImageWrapper = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MenuImage = styled.img`
+  width: 64px;
+  height: 58px;
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background: #dddddd;
+  margin: 30px 0;
+`;
