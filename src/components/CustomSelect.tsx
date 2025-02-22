02bc12a637310858eb7ed8c9c0cb544c3ebfa989
youminki <dbalsrl7648@gmail.com>
2025-02-23 04:02:52 +0900

    fix : Custom Select UI수정

--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -2,23 +2,23 @@
 import styled from 'styled-components';
 
 export const CustomSelect = styled.select`
-  /* 기본 select UI 제거 */
-  appearance: none;
-  -webkit-appearance: none; /* 사파리 호환 */
+  font-size: 16px;
+  border: 1px solid #000000;
+  border-radius: 4px;
+  height: 57px;
+  width: 100%;
+  padding: 0 40px 0 16px; /* 오른쪽에 아이콘 들어갈 공간 확보 */
+  font-family: 'NanumSquare Neo OTF';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 13px;
+  line-height: 14px;
 
-  /* 커스텀 화살표 이미지 */
+  color: #000000;
+  appearance: none;
   background: url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20width%3D'10'%20height%3D'6'%20viewBox%3D'0%200%2010%206'%20xmlns%3D'http%3A//www.w3.org/2000/svg'%3E%3Cpath%20d%3D'M0%200l5%206l5-6z'%20fill%3D'%23000'%20/%3E%3C/svg%3E")
     no-repeat right 16px center/10px 6px;
-  /* 배경색, 필요시 조정 */
-  background-color: #fff;
-
-  /* 드롭다운 화살표가 select 오른쪽에 보이도록 여유를 둠 */
-  padding-right: 35px;
-
-  border: 1px solid #000000;
-
   &:focus {
     outline: none;
-    border-color: #000000;
-  }
+    border-colo
 `;
