98805b59edd40c2535a04e0cc6d7c0b6af6d70ed
youminki <dbalsrl7648@gmail.com>
2025-03-16 19:29:47 +0900

    feat : Footer UI

--- src/components/Landing/LandingPage5.tsx
+++ src/components/Landing/LandingPage5.tsx
@@ -1,64 +1,102 @@
-// src/components/Landing/LandingPage5.tsx
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// src/components/Landing/LandingPage4.tsx
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import LastImg from '../../assets/Landing/LastImg.png';
-import FirstImg from '../../assets/Landing/LastbackgroundImg.svg';
+import { useNavigate } from 'react-router-dom';
+import LeftArrowIcon from '../../assets/Landing/left-arrow.svg';
+import RightArrowIcon from '../../assets/Landing/right-arrow.svg';
+import ScreenImg1 from '../../assets/Landing/ScreenImg1.svg';
+import ScreenImg2 from '../../assets/Landing/ScreenImg2.svg';
+import ScreenImg3 from '../../assets/Landing/ScreenImg3.svg';
+
+interface Screen {
+  img: string;
+  subtitle: React.ReactNode;
+}
+
+const screens: Screen[] = [
+  {
+    img: ScreenImg1,
+    subtitle: (
+      <>
+        인스타그램으로 나의 스타일 파악 후 <br />
+        브랜드 매칭하기
+      </>
+    ),
+  },
+  { img: ScreenImg2, subtitle: '노출 & 포스팅할 제품 선택하기' },
+  {
+    img: ScreenImg3,
+    subtitle: (
+      <>
+        콘텐츠 제작, 팔로워들과 소통하며 <br />
+        판매 수익 창출
+      </>
+    ),
+  },
+];
 
 const LandingPage5: React.FC = () => {
+  const [currentScreenIndex, setCurrentScreenIndex] = useState<number>(0);
+  const [imagesLoaded, setImagesLoaded] = useState<boolean>(false);
   const navigate = useNavigate();
 
-  const handleStartClick = () => {
-    navigate('/LandingDetail');
-  };
+  useEffect(() => {
+    let loadedImages = 0;
+    screens.forEach((screen) => {
+      const img = new Image();
+      img.src = screen.img;
+      img.onload = () => {
+        loadedImages += 1;
+        if (loadedImages === screens.length) {
+          setImagesLoaded(true);
+        }
+      };
+    });
+  }, []);
+
+  const handlePrevClick = () =>
+    setCurrentScreenIndex((prevIndex) =>
+      prevIndex === 0 ? screens.length - 1 : prevIndex - 1
+    );
+
+  const handleNextClick = () =>
+    setCurrentScreenIndex((prevIndex) =>
+      prevIndex === screens.length - 1 ? 0 : prevIndex + 1
+    );
 
   return (
     <Container>
-      <FirstImage src={FirstImg} alt='FirstImage page background' />
-      <LandingTitle4>
-        현재 <Yellow>N명</Yellow>의 인플루언서들이 <br /> melpik을 신청했어요!
-      </LandingTitle4>
-      <ButtonWrapper>
-        <MelpikStartButton onClick={handleStartClick}>
-          melpik 시작하기
-        </MelpikStartButton>
-      </ButtonWrapper>
-      <LandingSubtitle4>
-        사전예약 마감까지 <BoldText>N일 00:00</BoldText> 시간 남았어요!
-      </LandingSubtitle4>
-      <LastImage src={LastImg} alt='Last page background' />
-      <Footer>
-        <FooterContent>
-          <Line>
-            <FooterItem>
-              <FooterLabel>상호</FooterLabel>
-              <FooterSpan>멜픽(melpik)</FooterSpan>
-            </FooterItem>
-            <FooterItem>
-              <FooterLabel>대표</FooterLabel>
-              <FooterSpan>장용호</FooterSpan>
-            </FooterItem>
-            <FooterItem>
-              <FooterLabel>개인정보책임자</FooterLabel>
-              <FooterSpan>황민서</FooterSpan>
-            </FooterItem>
-          </Line>
-          <Line>
-            <FooterItem>
-              <FooterLabel>소재지</FooterLabel>
-              <FooterSpan>
-                서울시 금천구 디지털로9길 41, 1008호 (삼성IT해링턴타워)
-              </FooterSpan>
-            </FooterItem>
-          </Line>
-          <Line>
-            <FooterItem>
-              <FooterLabel>이메일</FooterLabel>
-              <FooterSpan>liftcomma@gmail.com</FooterSpan>
-            </FooterItem>
-          </Line>
-        </FooterContent>
-      </Footer>
+      <LandingTitle3>
+        <MelpicHighlight>melpik</MelpicHighlight>은 <br />
+        <BrandHighlight>어떤 서비스인가요?</BrandHighlight>
+      </LandingTitle3>
+      <LandingSubtitle3>
+        {screens[currentScreenIndex].subtitle}
+      </LandingSubtitle3>
+      <ScreenContainer>
+        <ArrowButton onClick={handlePrevClick}>
+          <ArrowIcon src={LeftArrowIcon} alt='Previous' />
+        </ArrowButton>
+        <ScreenImageContainer>
+          {imagesLoaded ? (
+            <ScreenImage
+              src={screens[currentScreenIndex].img}
+              alt='Screen'
+              loading='lazy'
+            />
+          ) : (
+            <LoadingSpinner />
+          )}
+        </ScreenImageContainer>
+        <ArrowButton onClick={handleNextClick}>
+          <ArrowIcon src={RightArrowIcon} alt='Next' />
+        </ArrowButton>
+      </ScreenContainer>
+      <PaginationDots>
+        {screens.map((_, idx) => (
+          <Dot key={idx} isActive={currentScreenIndex === idx} />
+        ))}
+      </PaginationDots>
     </Container>
   );
 };
@@ -66,135 +104,144 @@ const LandingPage5: React.FC = () => {
 export default React.memo(LandingPage5);
 
 const Container = styled.div`
-  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   width: 100%;
   padding: 60px 20px;
-  background-color: transparent;
-  min-height: calc(100vh - 100px);
+  background-color: #f5f5f5;
+  min-height: 932px;
   max-width: 600px;
   margin: 0 auto;
-  padding-bottom: 100px;
+  border-radius: 20px;
 `;
 
-const LandingTitle4 = styled.h1`
+const LandingTitle3 = styled.h1`
   font-family: 'NanumSquare Neo OTF', sans-serif;
-  font-weight: 400;
-  font-size: 17px;
+  font-weight: 700;
+  font-size: 24px;
   line-height: 30px;
   text-align: center;
-  margin-top: 50px;
-  opacity: 0.9;
+  margin-bottom: 50px;
   color: #000000;
 `;
 
-const Yellow = styled.span`
+const BrandHighlight = styled.span`
+  font-family: 'NanumSquare Neo OTF', sans-serif;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 30px;
+  text-align: center;
+  color: #f6ae24;
+`;
+
+const MelpicHighlight = styled.span`
   color: #f6ae24;
   font-family: 'NanumSquare Neo OTF', sans-serif;
-  font-weight: 800;
-  font-size: 20px;
+  font-weight: 700;
+  font-size: 24px;
   line-height: 30px;
+  text-align: center;
 `;
 
-const ButtonWrapper = styled.div`
-  margin-top: 33px;
-  margin-bottom: 9px;
-`;
-
-const MelpikStartButton = styled.button`
-  width: 300px;
-  height: 45px;
-  background-color: #f6ae24;
-  color: #ffffff;
-  border: none;
-  border-radius: 30px;
+const LandingSubtitle3 = styled.p`
   font-family: 'NanumSquare Neo OTF', sans-serif;
-  font-weight: 800;
-  font-size: 16px;
-  cursor: pointer;
-  transition: background-color 0.3s;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  text-align: center;
+  height: 60px;
+  margin-bottom: 32px;
+  color: #000000;
+`;
 
-  &:hover {
-    background-color: #e0a900;
+const ScreenContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  position: relative;
+  max-width: 800px;
+`;
+
+const ScreenImageContainer = styled.div`
+  width: 220px;
+  height: 466px;
+  background: #f0f0f0;
+  filter: drop-shadow(3px 5px 15px rgba(18, 18, 18, 0.15));
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const ScreenImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: opacity 0.5s ease-in-out;
+`;
+
+const ArrowButton = styled.button`
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  padding: 10px;
+  z-index: 1;
+
+  &:first-of-type {
+    left: 0;
+  }
+
+  &:last-of-type {
+    right: 0;
   }
 `;
 
-const LandingSubtitle4 = styled.h2`
-  font-family: 'NanumSquare Neo OTF', sans-serif;
-  font-size: 15px;
-  text-align: center;
-  color: #000000;
-  margin-bottom: 40px;
+const ArrowIcon = styled.img`
+  width: 24px;
+  height: 24px;
 `;
 
-const BoldText = styled.span`
-  font-weight: 800;
-  color: #000000;
-`;
-
-const FirstImage = styled.img`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  z-index: -1;
-`;
-
-const LastImage = styled.img`
-  width: 100%;
-  margin-top: auto;
-  object-fit: cover;
-  height: auto;
-`;
-
-const Footer = styled.footer`
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  background-color: #ffffff;
-  text-align: left;
-  padding: 28px 0;
-  font-family: 'NanumSquare Neo OTF', sans-serif;
-  font-size: 14px;
-  border-top: 1px solid #d3d3d3;
-  margin-bottom: 10px;
-`;
-
-const FooterContent = styled.div`
-  width: 100%;
-  padding: 0 20px;
-`;
-
-const Line = styled.div`
+const PaginationDots = styled.div`
   display: flex;
-  justify-content: space-between;
-  margin-bottom: 3px;
-  flex-wrap: nowrap;
-`;
-
-const FooterItem = styled.div`
-  display: flex;
+  justify-content: center;
   align-items: center;
-  margin-bottom: 3px;
-  flex: 1;
-  min-width: 100px;
+  gap: 10px;
+  margin-top: 30px;
 `;
 
-const FooterLabel = styled.label`
-  font-family: 'Noto Sans', sans-serif;
-  font-weight: 700;
-  font-size: 10px;
-  margin-right: 10px;
-  color: #000000;
+interface DotProps {
+  isActive: boolean;
+}
+
+const Dot = styled.div<DotProps>`
+  width: ${(props) => (props.isActive ? '20px' : '10px')};
+  height: 10px;
+  background-color: ${(props) => (props.isActive ? '#f5ab35' : '#d9d9d9')};
+  border-radius: 100px;
+  transition:
+    width 0.3s,
+    background-color 0.3s;
 `;
 
-const FooterSpan = styled.span`
-  font-family: 'Noto Sans', sans-serif;
-  font-weight: 400;
-  font-size: 10px;
-  color: #000000;
+const LoadingSpinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 5px solid #f0f0f0;
+  border-top: 5px solid #f5ab35;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  margin: 0 auto;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
