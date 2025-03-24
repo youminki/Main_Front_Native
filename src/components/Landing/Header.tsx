302913535741bdb5c3571b7436f154e68be0ece7
youminki <dbalsrl7648@gmail.com>
2025-03-25 00:36:24 +0900

    feat : Header UI

--- src/components/Landing/Header.tsx
+++ src/components/Landing/Header.tsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import LandingLogoIcon from '../../assets/Landing/LandingLogoIcon.svg';
+import ShareIcon from '../../assets/Landing/ShareIcon.svg';
 
 const Header: React.FC = () => {
   return (
     <HeaderContainer>
       <Logo src={LandingLogoIcon} alt='Landing Logo' />
+      <Icons src={ShareIcon} alt='Share Icon' />
     </HeaderContainer>
   );
 };
@@ -18,15 +20,24 @@ const HeaderContainer = styled.header`
   top: 0;
   left: 0;
   width: 100%;
-  height: 60px; /* 필요에 따라 높이 조정 */
+  height: 120px;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #ffffff; /* 배경색은 필요에 따라 수정 */
+  background-color: #ffffff;
   z-index: 1000;
 `;
 
 const Logo = styled.img`
-  height: 40px; /* 로고 높이 조정, 필요에 따라 수정 */
+  height: 24px;
   width: auto;
+  margin-top: 60px;
+`;
+
+const Icons = styled.img`
+  position: absolute;
+  right: 20px;
+  height: 20px;
+  width: auto;
+  margin-top: 60px;
 `;
