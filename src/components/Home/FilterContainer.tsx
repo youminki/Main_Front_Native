1bd88931470222be42a42942f0e39d5c4b88e66e
youminki <dbalsrl7648@gmail.com>
2025-04-22 22:59:25 +0900

    fix : Toggle button 주석처리

--- src/components/Home/FilterContainer.tsx
+++ src/components/Home/FilterContainer.tsx
@@ -3,38 +3,35 @@ import styled from 'styled-components';
 import FilterIcon from '../../assets/FilterIcon.svg';
 import FilterModal from '../../components/FilterModal';
 
-interface FilterContainerProps {
-  seasonToggle: boolean;
-  setSeasonToggle: (toggle: boolean) => void;
-}
+// interface FilterContainerProps {
+//   seasonToggle: boolean;
+//   setSeasonToggle: (toggle: boolean) => void;
+// }
 
-export const SeasonToggle = ({
-  isActive,
-  toggle,
-}: {
-  isActive: boolean;
-  toggle: () => void;
-}) => (
-  <StyledSeasonToggle onClick={toggle} $isActive={isActive}>
-    <ToggleCircle $isActive={isActive} />
-    <ToggleText $isActive={isActive}>{isActive ? '켜짐' : '꺼짐'}</ToggleText>
-  </StyledSeasonToggle>
-);
+// export const SeasonToggle = ({
+//   isActive,
+//   toggle,
+// }: {
+//   isActive: boolean;
+//   toggle: () => void;
+// }) => (
+//   <StyledSeasonToggle onClick={toggle} $isActive={isActive}>
+//     <ToggleCircle $isActive={isActive} />
+//     <ToggleText $isActive={isActive}>{isActive ? '켜짐' : '꺼짐'}</ToggleText>
+//   </StyledSeasonToggle>
+// );
 
-const FilterContainer: React.FC<FilterContainerProps> = ({
-  seasonToggle,
-  setSeasonToggle,
-}) => {
+const FilterContainer: React.FC = () => {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
 
   return (
     <Container>
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <SeasonToggle
+        {/* <SeasonToggle
           isActive={seasonToggle}
           toggle={() => setSeasonToggle(!seasonToggle)}
         />
-        <ToggleLabel>대여전용</ToggleLabel>
+        <ToggleLabel>대여전용</ToggleLabel> */}
       </div>
       {/* FilterIconContainer 클릭 시 모달 열기 */}
       <FilterIconContainer onClick={() => setIsFilterOpen(true)}>
@@ -59,45 +56,45 @@ const Container = styled.div`
   margin-bottom: 20px;
 `;
 
-const StyledSeasonToggle = styled.div<{ $isActive: boolean }>`
-  display: flex;
-  align-items: center;
-  position: relative;
-  width: 60px;
-  height: 30px;
-  background-color: ${({ $isActive }) => ($isActive ? '#222' : '#D9D9D9')};
-  border-radius: 15px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-`;
+// const StyledSeasonToggle = styled.div<{ $isActive: boolean }>`
+//   display: flex;
+//   align-items: center;
+//   position: relative;
+//   width: 60px;
+//   height: 30px;
+//   background-color: ${({ $isActive }) => ($isActive ? '#222' : '#D9D9D9')};
+//   border-radius: 15px;
+//   cursor: pointer;
+//   transition: background-color 0.3s ease;
+// `;
 
-const ToggleCircle = styled.div<{ $isActive: boolean }>`
-  position: absolute;
-  width: 28px;
-  height: 28px;
-  background: #fff;
-  border-radius: 50%;
-  left: ${({ $isActive }) => ($isActive ? '32px' : '2px')};
-  transition: left 0.3s ease;
-`;
+// const ToggleCircle = styled.div<{ $isActive: boolean }>`
+//   position: absolute;
+//   width: 28px;
+//   height: 28px;
+//   background: #fff;
+//   border-radius: 50%;
+//   left: ${({ $isActive }) => ($isActive ? '32px' : '2px')};
+//   transition: left 0.3s ease;
+// `;
 
-const ToggleText = styled.span<{ $isActive: boolean }>`
-  font-size: 10px;
-  color: #000;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  right: ${({ $isActive }) => ($isActive ? '6px' : 'unset')};
-  left: ${({ $isActive }) => (!$isActive ? '6px' : 'unset')};
-`;
+// const ToggleText = styled.span<{ $isActive: boolean }>`
+//   font-size: 10px;
+//   color: #000;
+//   position: absolute;
+//   top: 50%;
+//   transform: translateY(-50%);
+//   right: ${({ $isActive }) => ($isActive ? '6px' : 'unset')};
+//   left: ${({ $isActive }) => (!$isActive ? '6px' : 'unset')};
+// `;
 
-const ToggleLabel = styled.span`
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 13px;
-  color: #000000;
-  margin-left: 10px;
-`;
+// const ToggleLabel = styled.span`
+//   font-weight: 700;
+//   font-size: 14px;
+//   line-height: 13px;
+//   color: #000000;
+//   margin-left: 10px;
+// `;
 
 const FilterIconContainer = styled.div`
   display: flex;
@@ -105,14 +102,17 @@ const FilterIconContainer = styled.div`
   gap: 5px;
 
   font-weight: 700;
-  font-size: 14px;
+  font-size: 12px;
   line-height: 13px;
-  color: #000;
+  text-align: right;
+
+  color: #000000;
+
   cursor: pointer;
 
   img {
-    width: 24px;
-    height: 24px;
+    width: 22px;
+    height: 22px;
     padding: 8px;
     border-radius: 4px;
     border: 1px solid #ccc;
