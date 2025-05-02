a83b6ebcbd4b1757e6d7276fbb49d69d4cacfc57
youminki <dbalsrl7648@gmail.com>
2025-05-03 03:35:27 +0900

    fix : fix : dropdownMenu 수정 #2

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -165,7 +165,7 @@ const Home: React.FC = () => {
         <FilterContainer />
         {menuOpen && (
           <DropdownMenu>
-            {[3, 4, 5, 6].map((n) => (
+            {[1, 2, 3, 4, 5, 6].map((n) => (
               <DropdownItem
                 key={n}
                 active={viewCols === n}
