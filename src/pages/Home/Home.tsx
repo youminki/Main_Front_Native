7c23a9b24aa93e80d88b7d9857fb777b623dd66e
youminki <dbalsrl7648@gmail.com>
2025-05-03 03:00:04 +0900

    fix: DropdownMenu 위치수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -364,8 +364,8 @@ const DropdownToggle = styled.button`
 
 const DropdownMenu = styled.ul`
   position: absolute;
-  right: calc(80px + 0px);
-  top: calc(130px + 36px);
+  right: calc(60px + 0px);
+  top: calc(110px + 36px);
   background: #fff;
   border: 1px solid #ddd;
   border-radius: 4px;
